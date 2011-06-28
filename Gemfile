cf43837311a2cd5761ce47df8ef541cd48fc76fd
Tomas Lukosius <tomas@ebox.lt>
2011-06-28 09:56:30 +0100

    initial commit

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.9'
+gem 'mysql2', '~> 0.2.6'
+gem 'heroku'
+
+
+
+
+gem 'spree'
+gem 'spree_static_content'
+gem 'spree_editor', :git => "git://github.com/romul/spree_editor.git"
+
+# following gem is working under rails 3.0.7
+# gem 'spree_product_assembly'
+
+# Dev/Test gems
+group :development, :test do
+  gem 'webrat'
+  gem 'cucumber-rails'
+  gem 'rspec-rails'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ges::Application.config.secret_token = 'ae7355b06c3ab8e6f373f40ee1327ef51d73ee83a1c02e669b109737f5d944f7311f5aefed77c18bb7dcefc8e38d7546d14c1cb8babfc4e499860c4be92934ca'
